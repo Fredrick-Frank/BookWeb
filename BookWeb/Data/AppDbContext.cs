aeaa44a916869d62276dd69a2ee1f81446fab76e
Fredrick-Frank <igwefredrickchiemeka@gmail.com>
2022-03-17 02:44:47 +0100

    Add project files.

---
+++ BookWeb/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookWeb.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BookWeb/Views/Category/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookWeb.Views.Category
+{
+    public class EditModel : PageModel //the model is an edit-model
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
